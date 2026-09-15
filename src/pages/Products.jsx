491f8eddbdae9e754a998163efe8b9e1e60766d1
Skyghost12 <preciousonyeka2021@gmail.com>
2026-09-15 22:55:42 +0100

    fixed product page mobile view overflowing

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -47,7 +47,7 @@ function Product() {
   );
 
   return (
-    <main className="min-h-screen bg-slate-50 mt-8">
+    <main className="min-h-screen bg-slate-50 mt-8 overflow-hidden">
       <section className="bg-white">
         <div className="mx-auto max-w-7xl px-2 py-16 sm:px-6 lg:px-3">
           <div className="max-w-3xl">
@@ -69,7 +69,7 @@ function Product() {
       </section>
 
       <section className="bg-white flex justify-between items-center flex-col gap-4">
-        <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-3">
+        <div className="mx-auto max-w-7xl px-2 py-16 sm:px-6 lg:px-2 ">
           <div className="flex flex-col gap-4 items-center">
             <h1 className="text-2xl font-bold">Available Products</h1>
             <div className="relative">
@@ -106,7 +106,7 @@ function Product() {
               <button
                 key={number}
                 onClick={() => setCurrentPage(number)}
-                className={`px-3 py-2 rounded-lg ${currentPage === number
+                className={`px-2 py-2 rounded-lg ${currentPage === number
                   ? "bg-blue-600 text-white"
                   : "border"
                   }`}
