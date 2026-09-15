a3119237ea91da8bf85a5d9b20d6b4f9c64280f5
Skyghost12 <preciousonyeka2021@gmail.com>
2026-09-15 23:00:58 +0100

    adjusted the doctors rating

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -5,16 +5,16 @@ function DoctorCard({ doctor, onViewDetails }) {
         onClick={() => onViewDetails(doctor)}
         className="relative bg-white rounded-lg shadow-md p-6 m-4">
             <img src={doctor.image} alt={doctor.name} className="w-full h-80 object-cover hover:translate-0.5 hover:scale-105 transition" />
-            <div className="flex justify-between items-center mt-4">
-                <h3 className="font-bold text-lg">{doctor.name}</h3>
+            <div className="flex justify-between items-center mt-4 gap-4">
+                <h3 className="font-bold text-lg text-left">{doctor.name}</h3>
                 <p className="text-gray-600"> {doctor.available ?
                     <a className="bg-blue-500 text-white py-1.5 px-4 rounded hover:bg-blue-600">Contact</a> :
                     <span className="text-red-500">Not Available</span>
                 }</p>
             </div>
-            <div className="flex justify-between items-center mt-4">
-                <p className="text-gray-600">{doctor.specialization}</p>
-                <p className="text-gray-600">Experience: {doctor.experience}</p>
+            <div className="flex justify-between items-center mt-4 gap-4">
+                <p className="text-gray-600 text-left">{doctor.specialization}</p>
+                <p className="text-gray-600 text-left">Experience: {doctor.experience}</p>
             </div>
             <span className="absolute left-0 top-5 rounded-r-full bg-blue-600 px-3 py-1 text-sm text-white">
                 ⭐ {doctor.rating}
